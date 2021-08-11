713ac0525084422eff7d577b68063a19181c0bd0
2jaebbang <2jaebbang@naver.com>
2021-08-11 11:03:32 +0900

    orderForm 완성

--- src/main/resources/static/order.js
+++ src/main/resources/static/order.js
@@ -67,98 +67,101 @@ function order() {
 
 
                 // //beverage 사이즈
-                // let tdBevSize = document.createElement("td");
-                //
-                // let divS = document.createElement("div");
-                //
-                // divS.classList.add("btn-check");
-                // divS.setAttribute("role","group");
-                // divS.ariaLabel = "Basic radio toggle button group";
-                //
-                // let inputS1 = document.createElement("input");
-                // inputS1.type="radio";
-                // inputS1.classList.add("btn-check");
-                // inputS1.name="btnradioS";
-                // inputS1.id="btnradioS1";
-                // inputS1.autocomplete="off";
-                // document.getElementById("btnradio3").checked = "true";
-                //
-                // let inputS2 = document.createElement("input");
-                // inputS2.type="radio";
-                // inputS2.classList.add("btn-check");
-                // inputS2.name="btnradioS";
-                // inputS2.id="btnradioS2";
-                // inputS2.autocomplete="off";
-                //
-                // let inputS3 = document.createElement("input");
-                // inputS3.type="radio";
-                // inputS3.classList.add("btn-check");
-                // inputS3.name="btnradioS";
-                // inputS3.id="btnradioS3";
-                // inputS3.autocomplete="off";
-                //
-                // let labelS1 = document.createElement("label");
-                // labelS1.classList.add("btn");
-                // labelS1.classList.add("btn-outline-primary");
-                // labelS1.htmlFor = "btnradioS1";
-                // labelS1.innerText = "Tall";
-                //
-                // let labelS2 = document.createElement("label");
-                // labelS2.classList.add("btn");
-                // labelS2.classList.add("btn-outline-primary");
-                // labelS2.htmlFor = "btnradioS2";
-                // labelS2.innerText = "Grande";
-                //
-                // let labelS3 = document.createElement("label");
-                // labelS3.classList.add("btn");
-                // labelS3.classList.add("btn-outline-primary");
-                // labelS3.htmlFor = "btnradioS3";
-                // labelS3.innerText = "Venti";
-                //
-                // divS.appendChild(inputS1);
-                // divS.appendChild(labelS1);
-                // divS.appendChild(inputS2);
-                // divS.appendChild(labelS2);
-                // divS.appendChild(inputS3);
-                // divS.appendChild(labelS3);
-                // tdBevSize.appendChild(divS);
-                //
-                //
-                // //수량선택
-                // let tdAmount = document.createElement("td");
-                // let select = document.createElement("select");
-                //
-                // let amount = [];
-                // amount.push("수량선택");
-                //
-                // for (let i = 1; i <= 10; i++) {
-                //     let option = document.createElement("option");
-                //     amount.push(i);
-                //     select.appendChild(option);
-                //     option.innerText=amount[i];
-                // }
+                let tdBevSize = document.createElement("td");
 
-                // tdAmount.appendChild(select);
+                let divS = document.createElement("div");
+
+                divS.classList.add("btn-group");
+                divS.classList.add("btn-group-toggle");
+                divS.setAttribute("data-toggle","buttons");
+                tdBevSize.appendChild(divS);
+
+                let labelS1 = document.createElement("label");
+                labelS1.classList.add("btn");
+                labelS1.classList.add("btn-secondary");
+                labelS1.classList.add("active");
+                labelS1.innerText="Tall";
+                divS.appendChild(labelS1);
+
+                let inputS1 = document.createElement("input");
+                inputS1.type="radio";
+                inputS1.name="btnradioS";
+                inputS1.id="btnradioS1";
+                inputS1.autocomplete="off";
+
+                labelS1.appendChild(inputS1);
+
+                let labelS2 = document.createElement("label");
+                labelS2.classList.add("btn");
+                labelS2.classList.add("btn-secondary");
+                labelS2.classList.add("active");
+                labelS2.innerText="Grande";
+                divS.appendChild(labelS2);
+
+                let inputS2 = document.createElement("input");
+                inputS2.type="radio";
+                inputS2.name="btnradioS";
+                inputS2.id="btnradioS2";
+                inputS2.autocomplete="off";
+
+                labelS2.appendChild(inputS2);
+
+                let labelS3 = document.createElement("label");
+                labelS3.classList.add("btn");
+                labelS3.classList.add("btn-secondary");
+                labelS3.classList.add("active");
+                labelS3.innerText="Venti";
+                divS.appendChild(labelS3);
+
+                let inputS3 = document.createElement("input");
+                inputS3.type="radio";
+                inputS3.name="btnradioS";
+                inputS3.id="btnradioS3";
+                inputS3.autocomplete="off";
+
+                labelS3.appendChild(inputS3);
+
+
+
+                 //수량선택
+                 let tdAmount = document.createElement("td");
+                 let select = document.createElement("select");
+                 select.classList="custom-select";
+                 select.style="width: 100px";
+                 let amount = [];
+                 amount.push("수량선택");
+
+                for (let i = 1; i <= 10; i++) {
+                    let option = document.createElement("option");
+                    option.value= `${i}`;
+                    amount.push(i);
+                    select.appendChild(option);
+                    option.innerText=amount[i];
+                }
+
+                 tdAmount.appendChild(select);
 
 
                 //ok버튼
-                // let tdOk = document.createElement("td");
-                // let btn = document.createElement("button");
-                // tdOk.classList.add("btn-group");
-                // tdOk.appendChild(btn);
+                let tdOk = document.createElement("td");
+                let btn = document.createElement("button");
+                btn.classList.add("btn");
+                btn.classList.add("btn-success");
+                btn.innerText="주문";
+                tdOk.appendChild(btn);
 
 
 
                 if(data[i]['category']==="beverage"){
                     trBev.appendChild(tdName);
                     trBev.appendChild(tdBevTemp);
-                    // trBev.appendChild(tdBevSize);
-                    // trBev.appendChild(tdAmount);
-                    // trBev.appendChild(tdOk);
+                    trBev.appendChild(tdBevSize);
+                    trBev.appendChild(tdAmount);
+                    trBev.appendChild(tdOk);
                 } else {
                     trFood.appendChild(tdName);
-                    // trFood.appendChild(tdAmount);
-                    // trFood.appendChild(tdOk);
+                    trFood.appendChild(tdAmount);
+                    trFood.appendChild(tdOk);
                 }
 
                 document.getElementById("beverageTable").appendChild(trBev);
